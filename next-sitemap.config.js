9e98304868271c59d350c2f8c09687c39fc6de2d
Ashraf Fatema <ashraf@Ashrafs-Mac-mini.local>
2025-11-18 18:51:23 +0530

    SEO tried

---
+++ next-sitemap.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://azizmanva-com-preview.vercel.app',
+  generateRobotsTxt: false, // because you're serving one from /public
+  changefreq: 'weekly',
+  priority: 0.7,
+  additionalPaths: async (config) => {
+    const { default: notesData } = await import('./data/notesData');
+    return notesData.map((note) => ({
+      loc: `/notes/${note.slug}`,
+      changefreq: 'monthly',
+      priority: 0.8,
+    }));
+  },
+};
